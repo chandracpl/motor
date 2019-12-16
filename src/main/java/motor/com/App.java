a078807d2b1235b2744ccfeed2d1c552011c4a93
root <root@ip-172-31-91-146.ec2.internal>
2019-12-16 12:59:22 +0000

    motor.com

---
+++ src/main/java/motor/com/App.java
@@ -0,0 +1,13 @@
+package motor.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
